ff7fccb06c59753104e2494fb5cefa9ad21717b0
Jacob Sorensen <js@gmail.com>
2018-10-08 19:00:30 +0200

    models added

---
+++ Models/vare.cs
@@ -0,0 +1,14 @@
+using MongoDB.Bson;
+
+namespace ecomm1.Models
+{
+    public class vare
+    {
+        public ObjectId _id { get; set; }
+        public int VareId { get; set; }
+        public string Beskrivelse { get; set; }
+        public int Pris { get; set; }
+        public bool Udsolgt { get; set; }
+
+    }
+}
