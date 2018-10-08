741424f3e533d72858941fa95f0de34051add218
Jacob Sorensen <js@gmail.com>
2018-10-08 19:59:05 +0200

    varenavn i varemodel samt bin i git ignore

--- Models/vare.cs
+++ Models/vare.cs
@@ -6,6 +6,7 @@ namespace ecomm1.Models
     {
         public ObjectId _id { get; set; }
         public int VareId { get; set; }
+        public string Navn { get; set; }
         public string Beskrivelse { get; set; }
         public int Pris { get; set; }
         public bool Udsolgt { get; set; }
